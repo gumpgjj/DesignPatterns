59155370d44dd6d7dba1a18f9fc5c81bb150d467
gaojj <gaojj@fastcnt.com>
2014-04-17 12:39:19 +0800

    abstract method example

---
+++ DesignPatters/src/com/megamind/ben/Sample2.java
@@ -0,0 +1,10 @@
+package com.megamind.ben;
+
+/**
+ * Created by gaojj
+ * on 2014/4/17.
+ * on ${Time}
+ * Good Life One Day
+ */
+public class Sample2 {
+}
--- DesignPatters/src/com/megamind/creational/DemoFactory.java
+++ DesignPatters/src/com/megamind/creational/DemoFactory.java
@@ -2,6 +2,10 @@ package com.megamind.creational;
 
 /**
  * Created by Administrator on 2014/4/17.
+ *
+ * Thinking java
+ *
+ * Gof
  */
 public class DemoFactory {
     public static void main(String args[]){
