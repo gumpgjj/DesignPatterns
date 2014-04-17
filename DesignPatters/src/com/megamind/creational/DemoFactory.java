a8a41662cdf5681374b11bd611906d11d6d8d702
gaojj <gaojj@fastcnt.com>
2014-04-17 11:31:53 +0800

    add first test method

--- DesignPatters/src/com/megamind/creational/DemoFactory.java
+++ DesignPatters/src/com/megamind/creational/DemoFactory.java
@@ -4,4 +4,7 @@ package com.megamind.creational;
  * Created by Administrator on 2014/4/17.
  */
 public class DemoFactory {
+    public static void main(String args[]){
+        System.out.println("hello git hub");
+    }
 }
