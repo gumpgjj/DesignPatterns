f7b055464fa449ceab0e8f8a2852eb9b5a4dc9c9
gaojj <gaojj@fastcnt.com>
2014-04-18 09:38:34 +0800

    static factory

--- DesignPatters/src/com/megamind/test/SenderFactoryTest.java
+++ DesignPatters/src/com/megamind/test/SenderFactoryTest.java
@@ -1,6 +1,7 @@
 package com.megamind.test;
 
 import com.megamind.creational.SenderFactory;
+import com.megamind.creational.SenderStaticFactory;
 import com.megamind.interfaces.Sender;
 
 import java.util.Random;
@@ -14,17 +15,22 @@ import java.util.Random;
 public class SenderFactoryTest {
     public static void main(String args[]) {
         SenderFactory senderFactory = new SenderFactory();
+        SenderStaticFactory senderStaticFactory = new SenderStaticFactory();
         for (int i = 0; i < 20; i++) {
             System.out.print(i);
             Sender sender = null;
+            Sender sender1 =null;
             Random random = new Random();
             int randomInt = random.nextInt();
             if (randomInt % 2 == 0) {
                 sender = senderFactory.produce("mail");
+                sender1 = senderStaticFactory.produceSms();
             } else {
                 sender = senderFactory.produce("sms");
+                sender1 = senderStaticFactory.produceMail();
             }
             sender.send();
+            sender1.send();
         }
     }
 }
