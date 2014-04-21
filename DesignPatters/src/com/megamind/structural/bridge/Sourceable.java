488e80df4160b243bb7203d2575397d7bb48d034
gaojj <gaojj@fastcnt.com>
2014-04-21 10:49:02 +0800

    bridge 接口和实现的分离

---
+++ DesignPatters/src/com/megamind/structural/bridge/Sourceable.java
@@ -0,0 +1,11 @@
+package com.megamind.structural.bridge;
+
+/**
+ * Created by megamind
+ * on 2014/4/21.
+ * on 10:38
+ * Good Life One Day
+ */
+public interface Sourceable {
+    void method();
+}
