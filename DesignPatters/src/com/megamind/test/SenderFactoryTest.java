e06fe684760498486c351cdcd53295ea7985ad64
gaojj <gaojj@fastcnt.com>
2014-04-22 19:08:38 +0800

    文件的测试

--- DesignPatters/src/com/megamind/test/SenderFactoryTest.java
+++ DesignPatters/src/com/megamind/test/SenderFactoryTest.java
@@ -1,11 +1,13 @@
 package com.megamind.test;
 
 import com.megamind.creational.SenderStaticFactory;
+import com.megamind.creational.factory.abstracts.SenderMailFactory;
 import com.megamind.creational.factory.simple.SenderFactory;
 import com.megamind.interfaces.Provider;
 import com.megamind.interfaces.Sender;
 import org.junit.Test;
-import com.megamind.creational.factory.abstracts.SenderMailFactory;
+
+import java.io.*;
 import java.util.Random;
 
 /**
@@ -45,4 +47,63 @@ public class SenderFactoryTest {
         sender.send();
     }
 
+    /**
+     * gbk to utf-8
+     */
+    @Test
+    public void testFileEncodingTransfer() throws Exception {
+
+        new FileOption().listFile("E:\\WeChatP\\WeChatPlat");
+        /*for (File file : files) {
+            FileOption fileOption = new FileOption();
+            fileOption.newFile(file);
+        }*/
+        /**
+         *
+         */
+    }
+
+    class FileOption {
+        public String fileInput(File file) throws IOException {
+            StringBuilder stringBuilder = new StringBuilder();
+            InputStreamReader isr = new InputStreamReader(new FileInputStream(
+                    file), "GBK");
+            int ch = 0;
+            while ((ch = isr.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+            return stringBuilder.toString();
+        }
+
+        public void fileOut(File file, String content) throws Exception {
+            OutputStreamWriter osw = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+            osw.write(content, 0, content.length());
+            osw.flush();
+        }
+
+        public void listFile(String file) throws Exception{
+            File file_dir = new File(file);
+
+
+            String filename[] = file_dir.list();
+
+
+            for (int i = 0; i < filename.length; i++) {
+                File ft = new File(file + "//" + filename[i]);
+
+                if (filename[i].endsWith(".java") || filename[i].endsWith(".jsp") || filename[i].endsWith(".xml") || filename[i].endsWith(".txt") || filename[i].endsWith(".html") || filename[i].endsWith(".css") || filename[i].endsWith(".sql") || filename[i].endsWith(".properties")) {
+                    fileOut(ft,fileInput(ft));
+                }
+                if (ft.isDirectory()) {
+
+
+                    listFile(file + "//" + filename[i]);
+
+
+                }
+            }
+
+
+        }
+    }
 }
