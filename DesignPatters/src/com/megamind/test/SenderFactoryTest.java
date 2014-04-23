9195c6301bf7fa54ddaf89db55e2c683c586c5cb
gaojj <gaojj@fastcnt.com>
2014-04-23 13:49:36 +0800

    jar file process

--- DesignPatters/src/com/megamind/test/SenderFactoryTest.java
+++ DesignPatters/src/com/megamind/test/SenderFactoryTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 
 import java.io.*;
 import java.util.Random;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
 
 /**
  * Created by gaojj
@@ -81,7 +83,7 @@ public class SenderFactoryTest {
             osw.flush();
         }
 
-        public void listFile(String file) throws Exception{
+        public void listFile(String file) throws Exception {
             File file_dir = new File(file);
 
 
@@ -92,7 +94,7 @@ public class SenderFactoryTest {
                 File ft = new File(file + "//" + filename[i]);
 
                 if (filename[i].endsWith(".java") || filename[i].endsWith(".jsp") || filename[i].endsWith(".xml") || filename[i].endsWith(".txt") || filename[i].endsWith(".html") || filename[i].endsWith(".css") || filename[i].endsWith(".sql") || filename[i].endsWith(".properties")) {
-                    fileOut(ft,fileInput(ft));
+                    fileOut(ft, fileInput(ft));
                 }
                 if (ft.isDirectory()) {
 
@@ -105,5 +107,40 @@ public class SenderFactoryTest {
 
 
         }
+    }
+
+    @Test
+    public void testJarMaven() throws Exception {
+        String path = "E:\\WeChatP\\WeChatPlat\\WEB-INF\\lib";
+        File file = new File(path);
+        String[] fileNames = file.list();
+        StringBuffer stringBuffer = new StringBuffer();
+        for (String one : fileNames) {
+            JarFile jarFile = new JarFile(path + File.separator + one);
+            Manifest manifest = jarFile.getManifest();
+            //System.out.println("====="+ manifest.getMainAttributes().getValue("Manifest-Version"));
+//            Enumeration  enumeration = jarFile.entries();
+//            while (enumeration.hasMoreElements()) {
+//                JarEntry entry = (JarEntry)enumeration.nextElement();
+//                String name = entry.getName();
+//                if(name.equals(JarFile.MANIFEST_NAME)){
+//                    Attributes attributes = entry.getAttributes();
+//                    System.out.println(entry.toString());
+//                    System.out.println(attributes);
+//                    //System.out.println(attributes.getValue("Manifest-Version"));
+//                }
+//            }
+            jarFile.close();
+            //String groupId =
+            stringBuffer.append(" <dependency>\n" +
+                    "            <groupId>" + one.replace(".jar", "") + "</groupId>\n" +
+                    "            <artifactId>" + one.replace(".jar", "") + "</artifactId>\n" +
+                    "            <version>1.0</version>\n" +
+                    "            <scope>system</scope>\n" +
+                    "            <systemPath>${project.basedir}/src/main/webapp/WEB-INF/lib/" + one + "</systemPath>\n" +
+                    "        </dependency>\n");
+
+        }
+        System.out.println(stringBuffer.toString());
     }
 }
