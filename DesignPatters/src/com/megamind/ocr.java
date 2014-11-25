5ba2cd8f78b975801a3c7f47a737c9324ee003e9
gumpgjj <123456google>
2014-11-25 09:21:13 +0800

    去掉ocr

--- DesignPatters/src/com/megamind/ocr.java
+++ DesignPatters/src/com/megamind/ocr.java
@@ -1,6 +1,6 @@
 package com.megamind;
 
-import net.sourceforge.tess4j.Tesseract;
+//import net.sourceforge.tess4j.Tesseract;
 import org.junit.Test;
 
 import java.io.File;
@@ -15,11 +15,11 @@ public class ocr {
     @Test
     public void userOcr(){
         File imageFile = new File("C:\\Users\\Administrator\\Desktop\\信贷系统录入界面\\个人业务\\一、新增申请\\7.bmp");
-        Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
+        //Tesseract instance = Tesseract.getInstance();  // JNA Interface Mapping
         // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
 
         try {
-            String result = instance.doOCR(imageFile);
+            String result = "";//instance.doOCR(imageFile);
             System.out.println(result);
         } catch (Exception e) {
             e.printStackTrace();
